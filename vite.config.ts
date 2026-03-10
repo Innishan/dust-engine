b8200d3711650a6fcb44e48089a28645ecd79a07
Innishan <inthishaninni@gmail.com>
2026-03-10 19:21:03 +0530

    fix tailwind build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 import { nodePolyfills } from "vite-plugin-node-polyfills"
 
 export default defineConfig({
   base: "/",
   plugins: [
     react(),
+    tailwindcss(),
     nodePolyfills()
   ],
   server: {
