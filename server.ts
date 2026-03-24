0654466f0aed6dd77df0e5133a225df058db3344
Innishan <inthishaninni@gmail.com>
2026-03-24 15:51:36 +0530

    fix render port

--- server.ts
+++ server.ts
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename);
 
 async function startServer() {
   const app = express();
-  const PORT = 4000;
+  const PORT = process.env.PORT || 4000;  
   
   app.use(cors());
   app.use(express.json());
